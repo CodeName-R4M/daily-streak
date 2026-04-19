d43e89fef271fd0a3a8b69274ff55e1025b00e9b
Sriram N <sriramn20060126@gmail.com>
2026-04-19 22:48:54 +0530

    day25 done

---
+++ day25/js-day25/soln.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    let map = {};
+
+    for (let obj of arr1) {
+        map[obj.id] = obj;
+    }
+
+    for (let obj of arr2) {
+        if (map[obj.id]) {
+            map[obj.id] = { ...map[obj.id], ...obj };
+        } else {
+            map[obj.id] = obj;
+        }
+    }
+
+    return Object.values(map).sort((a, b) => a.id - b.id);
+};
