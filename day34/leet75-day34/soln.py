ea0c56754980932e48b687b62fb0b10c75d0dfb0
Sriram N <sriramn20060126@gmail.com>
2026-05-03 21:33:37 +0530

    day34 python done

---
+++ day34/leet75-day34/soln.py
@@ -0,0 +1,15 @@
+class Solution:
+    def goodNodes(self, root):
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+            
+            good = 0
+            if node.val >= max_so_far:
+                good = 1
+            
+            max_so_far = max(max_so_far, node.val)
+            
+            return good + dfs(node.left, max_so_far) + dfs(node.right, max_so_far)
+        
+        return dfs(root, root.val)
